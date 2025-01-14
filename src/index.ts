8217b6adc828b08b8b7037f358fdb3d6de267a48
Maor.Levinshtein <maor.levinshtein@payboxapp.com>
2025-01-14 14:29:07 +0200

    init files

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import express, { Request, Response } from 'express';
 import mainRoutes from './routes/main_routes';
+import {MAIN_PORT} from "./configs/config";
 
 const app = express();
-const port = 3000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Use the main routes
 app.use('/main', mainRoutes);
 
-// Basic route
 app.get('/firstReq', (req: Request, res: Response) => {
   res.json({ message: 'Happy developing ✨' });
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(MAIN_PORT, () => {
+  console.log(`Server is running on http://localhost:${MAIN_PORT}`);
 });
