248dc5f1e6460640bb938b5b0899f4249794403d
Maor.Levinshtein <maor.levinshtein@payboxapp.com>
2025-01-14 15:40:07 +0200

    fix pkg json

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -2,16 +2,15 @@ import logger from '../configs/logger';
 
 export default class Main {
     constructor() {
-        console.log('Main constructor');
     }
 
     mainFunction(num: number) {
-        logger.info(`Main function, number: ${num} and also ${num + 1}`);
+        logger.info(`[main_controller](mainFunction)Main function, number: ${num} and also ${num + 1}`);
         return `Main function, number: ${num} and also ${num + 1}`;
     }
 
     calculate(num1: number, num2: number) {
-        logger.info(`Calculate function, num1: ${num1}, num2: ${num2} and result: ${num1 + num2}`);
+        logger.info(`[main_controller](calculate)Calculate function, num1: ${num1}, num2: ${num2} and result: ${num1 + num2}`);
         return `Calculate function, num1: ${num1}, num2: ${num2} and result: ${num1 + num2}`;
     }
 
