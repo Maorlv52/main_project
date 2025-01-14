2d8ac7f3c0f8bf162f8b4b81517999cabf7e860b
Maor.Levinshtein <maor.levinshtein@payboxapp.com>
2025-01-14 14:40:17 +0200

    init logger (winston)

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,9 +1,12 @@
+import logger from '../configs/logger';
+
 export default class Main {
     constructor() {
         console.log('Main constructor');
     }
 
     mainFunction(num: number) {
+        logger.info(`Main function, number: ${num} and also ${num + 1}`);
         return `Main function, number: ${num} and also ${num + 1}`;
     }
 
--- src/routes/main_routes.ts
+++ src/routes/main_routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import Main from '../controllers/main';
+import logger from '../configs/logger';
 
 const router = Router();
 const main = new Main();
@@ -7,6 +8,7 @@ const main = new Main();
 router.get('/about', (req: Request, res: Response) => {
 
   const result = main.mainFunction(1);
+  logger.info(`result: ${result}`);
   res.json({ message: result });
 });
 
