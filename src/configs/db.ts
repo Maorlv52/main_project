403037746c7a28fb93b3be7b5758ba1c67f49179
Maor.Levinshtein <maor.levinshtein@payboxapp.com>
2025-01-14 16:12:49 +0200

    mongodb

---
+++ src/configs/db.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import logger from "./logger";
+// import logger from '../logger.js';
+
+const connectDB = async () => {
+    const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/main';
+
+    try {
+        await mongoose.connect(mongoURI, {
+            serverSelectionTimeoutMS: 10000, // Wait for MongoDB to respond for 10 seconds
+        });
+        logger.info(`MongoDB connected successfully to: ${mongoURI}`);
+    } catch (err) {
+        if (err instanceof Error) {
+            logger.error(`MongoDB connection error: ${err.message}`);
+        } else {
+            logger.error('An unknown error occurred while connecting to MongoDB');
+        }
+        setTimeout(connectDB, 5000);
+    }
+};
+
+// Automatically call connectDB to establish the connection
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,41 @@
 import express, { Request, Response } from 'express';
 import mainRoutes from './routes/main_routes';
+import connectDB from './configs/db';
 import {MAIN_PORT} from "./configs/config";
+import logger from "./configs/logger";
 
 const app = express();
+// connectDB();
+//
+// app.use(express.json());
+//
+// app.use('/main', mainRoutes);
+//
+// app.get('/firstReq', (req: Request, res: Response) => {
+//   res.json({ message: 'Happy developing ✨' });
+// });
+//
+// app.listen(MAIN_PORT, () => {
+//   console.log(`Server is running on http://localhost:${MAIN_PORT}`);
+// });
 
-app.use(express.json());
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.use(express.json());
 
-app.use('/main', mainRoutes);
+    app.use('/main', mainRoutes);
 
-app.get('/firstReq', (req: Request, res: Response) => {
-  res.json({ message: 'Happy developing ✨' });
-});
+    app.get('/firstReq', (req: Request, res: Response) => {
+      res.json({ message: 'Happy developing ✨' });
+    });
 
-app.listen(MAIN_PORT, () => {
-  console.log(`Server is running on http://localhost:${MAIN_PORT}`);
-});
+    app.listen(MAIN_PORT, () => {
+      logger.info(`Server is running on http://localhost:${MAIN_PORT}`);
+    });
+  } catch (error) {
+    logger.error('Failed to start server:', error);
+  }
+};
+
+startServer();
